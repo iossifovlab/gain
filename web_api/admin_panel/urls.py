594c67d540add3aef35ba788715213a48a68aee8
IvoTod <ivo.tod.todorov@gmail.com>
2026-05-13 10:15:07 +0300

    Add new views to the admin panel

--- web_api/admin_panel/urls.py
+++ web_api/admin_panel/urls.py
@@ -5,4 +5,6 @@ from admin_panel import views
 urlpatterns = [
     path("admin-panel/reset-daily-quota", views.reset_daily_quota),
     path("admin-panel/reset-monthly-quota", views.reset_monthly_quota),
+    path("admin-panel/set-extra-quota", views.set_extra_quota),
+    path("admin-panel/set-current-quota", views.set_current_quota),
 ]
