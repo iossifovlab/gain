e83273a250ed244b786734012f035da6d9d3f1cd
IvoTod <ivo.tod.todorov@gmail.com>
2026-05-15 16:06:55 +0300

    Update admin panel views so that IP setting doesn't break

--- web_api/admin_panel/urls.py
+++ web_api/admin_panel/urls.py
@@ -3,10 +3,10 @@ from django.urls import path
 from admin_panel import views
 
 urlpatterns = [
-    path("admin-panel/reset-daily-quota", views.reset_daily_quota),
-    path("admin-panel/reset-monthly-quota", views.reset_monthly_quota),
-    path("admin-panel/set-extra-quota", views.set_extra_quota),
-    path("admin-panel/set-current-quota", views.set_current_quota),
-    path("admin-panel/set-session-quota", views.set_session_quota),
-    path("admin-panel/set-ip-quota", views.set_ip_quota),
+    path("admin-panel/reset-daily-quota", views.ResetDailyQuotaView.as_view()),
+    path("admin-panel/reset-monthly-quota", views.ResetMonthlyQuotaView.as_view()),
+    path("admin-panel/set-extra-quota", views.SetExtraQuotaView.as_view()),
+    path("admin-panel/set-current-quota", views.SetCurrentQuotaView.as_view()),
+    path("admin-panel/set-session-quota", views.SetSessionQuotaView.as_view()),
+    path("admin-panel/set-ip-quota", views.SetIpQuotaView.as_view()),
 ]
